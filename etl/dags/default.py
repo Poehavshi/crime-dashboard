f28b07b06798e0df7471574099b7d2d406c52276
arkadiysotnikov <cotnikoarkady@gmail.com>
2022-11-21 22:58:59 +0400

    Create example DAG

--- etl/dags/default.py
+++ etl/dags/default.py
@@ -1,1 +1,24 @@
 # test dag here
+
+import os
+import time
+from datetime import datetime, timedelta
+
+from airflow import DAG
+from airflow.operators.python import PythonOperator
+
+# default arguments
+default_args = {
+    'owner': 'dw',
+    'start_date': datetime(2021, 5, 9),
+    'email': [''],
+    'email_on_failure': False,
+    'email_on_retry': False,
+    "retries": 3,
+    "retry_delay": timedelta(minutes=1)
+}
+
+# * timetable DAG
+dag_timetable = DAG('timetable',
+                    description='Fetch timetable data hourly', catchup=False, schedule_interval="@hourly",
+                    default_args=default_args)
